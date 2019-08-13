744d8f3931382d980184d356c17d38a7f8d48f97
David Sousa <xipiro15@gmail.com>
2019-08-13 16:21:35 +0000

    accountService impl

---
+++ insane/src/main/java/org/academiadecodigo/codezillas/insane/services/AccountService.java
@@ -0,0 +1,26 @@
+package org.academiadecodigo.codezillas.insane.services;
+
+import org.academiadecodigo.codezillas.insane.persistence.model.User;
+
+import java.util.List;
+import java.util.Map;
+
+public class AccountService implements  UserService{
+
+    Map<Integer,String> usersAccounts;
+
+
+
+    public void delete(User user) {
+
+    }
+
+    public void saveOrUpdate() {
+
+        if (usersAccounts
+    }
+
+    public List<User> findBySkill() {
+        return null;
+    }
+}
--- insane/src/main/java/org/academiadecodigo/codezillas/insane/services/UserService.java
+++ insane/src/main/java/org/academiadecodigo/codezillas/insane/services/UserService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface UserService {
 
-    void delete();
-    void saveOrUpdate();
-    List<User> findBySkill();
+    void delete(User user);
+    void saveOrUpdate(User user);
+    List<User> findBySkill(String skill);
 }
