25885857673b872363c33278bf20a9ce3332340c
Milton Cardoso <miltonamc@gmail.com>
2019-08-14 01:43:23 +0000

    user service fixes

--- insane/src/main/java/org/academiadecodigo/codezillas/insane/controllers/CustomerController.java
+++ insane/src/main/java/org/academiadecodigo/codezillas/insane/controllers/CustomerController.java
@@ -5,17 +5,31 @@ import org.academiadecodigo.codezillas.insane.dtos.UserDtoToUser;
 import org.academiadecodigo.codezillas.insane.dtos.UserToUserDto;
 import org.academiadecodigo.codezillas.insane.persistence.model.User;
 import org.academiadecodigo.codezillas.insane.services.UserService;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.awt.image.BufferedImage;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -25,6 +39,7 @@ import java.util.Set;
 public class CustomerController {
 
     private UserService userService;
+    private boolean isMultipart;
 
     private UserToUserDto userToDto;
     private UserDtoToUser userDtoToUser;
@@ -67,15 +82,59 @@ public class CustomerController {
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "/recruiter")
-    public String register(@Valid @ModelAttribute("user") UserDto userDto, BindingResult bindingResult, Res) {
+    public String register(@Valid @ModelAttribute("user") UserDto userDto, BindingResult bindingResult) {
 
        if (bindingResult.hasErrors()) {
             return "404";
        }
 
+        //isMultipart = ServletFileUpload.isMultipartContent(user);
+        //DiskFileItemFactory diskFileItemFactory = new DiskFileItemFactory();
+
         userService.saveOrUpdate(userDtoToUser.user(userDto));
         //HttpSession httpSession =
         return "redirect:homepage";
+    }
+
+
+
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public String submit(@RequestParam("file") MultipartFile file, ModelMap modelMap, HttpSession session) {
+        modelMap.addAttribute("file", file);
+        //System.out.println("\n" + file.getSize()+ "----------------------\n");
+
+        try {
+            //ServletContext servletContext = session.getServletContext();
+            //String path = servletContext.getRealPath("WEB-INF/template/");
+            byte[] barr =file.getBytes();
+            System.out.println(file.getOriginalFilename() + "----------------------");
+            BufferedOutputStream bu = new BufferedOutputStream(new FileOutputStream(file.getOriginalFilename()));
+            bu.write(barr);
+            bu.flush();
+            bu.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "fileUploadView";
+    }
+
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.GET)
+    public String sub(Model model){
+
+        return "fileUploadView";
+    }
+
+    @RequestMapping(value = "/file", method = RequestMethod.GET)
+    public String sub2(Model model){
+        return "file";
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(100000);
+        return multipartResolver;
     }
     /*
 
